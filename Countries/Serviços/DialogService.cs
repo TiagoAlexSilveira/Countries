a110c1c98f5fd7878b613f0ad2185d1b672c0747
Tiago Silveira <tiago.alexandre.silveira@formandos.cinel.pt>
2020-04-20 18:59:27 +0100

    Added Classes

---
+++ Countries/Serviços/DialogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Countries.Serviços
+{
+    public class DialogService
+    {
+        public void ShowMessage(string title, string message)
+        {
+            MessageBox.Show(message, title);
+        }
+    }
+}
---
+++ Countries/Serviços/NetworkService.cs
@@ -0,0 +1,37 @@
+﻿using Countries.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countries.Serviços
+{
+    public class NetworkService //só tem 1 método para ver se estamos liagados à internet
+    {
+        public Response CheckConnection() // método para ver se temos conexão à internet
+        {
+            var client = new WebClient();
+
+            try  //vai tentar ligar à internet
+            {
+                using (client.OpenRead("http://clients3.google.com/generate_204")) // se o ping a este website correr bem
+                {
+                    return new Response //retorna uma reposta nova
+                    {
+                        IsSucess = true
+                    };
+                }
+            }
+            catch //se não conseguir ligar à internet
+            {
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = "Configure a sua ligação à Internet"
+                };
+            }
+        }
+    }
+}
